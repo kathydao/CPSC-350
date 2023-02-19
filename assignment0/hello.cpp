ca3ed611a5299af472500045f5c9de1e0153313c
KD <121912463+kathydao@users.noreply.github.com>
2023-02-18 23:03:02 -0800

    Add files via upload

---
+++ assignment0/hello.cpp
@@ -0,0 +1,12 @@
+#include "hello.h"
+#include <iostream>
+using namespace std;
+
+HelloWorld::HelloWorld(){
+  cout << "Hello World!" << endl;
+}
+HelloWorld::~HelloWorld(){
+  cout << "Hello World!" << endl;
+}
+
+
--- assignment0/hello.h
+++ assignment0/hello.h
@@ -1,1 +1,11 @@
+#ifndef HELLOWORLD_H_ 
+#define HELLOWORLD_H_
 
+#include <string>
+class HelloWorld{
+  public:
+    HelloWorld();
+    ~HelloWorld();
+}; 
+
+#endif
